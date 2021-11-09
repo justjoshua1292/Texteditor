3b21a09626e8226c57efccb5c2e4516c0bc8e4ff
JustJoshua <justjoshua@gmail.com>
2021-11-08 21:27:52 -0500

    changes and updates made in webpack.config.js file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html", 
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text editor by JOSHUA',
+        short_name: 'J.O.S.H.',
+        description: 'Text for dummies!',
+        start_url: '/',
+        publicPath: '/',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.m?js$/,
+          exclude:/node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"], plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            },
+          },
+        },
       ],
     },
   };
